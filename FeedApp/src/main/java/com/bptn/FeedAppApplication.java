3847c5fca84621cc2174994339aa4bcca9176704
DEDANDEUS <114673700+DEDANDEUS@users.noreply.github.com>
2022-11-03 05:57:48 -0700

    Committing FeedApp

---
+++ FeedApp/src/main/java/com/bptn/FeedAppApplication.java
@@ -0,0 +1,17 @@
+package com.bptn;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class FeedAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FeedAppApplication.class, args);
+		
+	}
+}
+
+
+
+
+
+
