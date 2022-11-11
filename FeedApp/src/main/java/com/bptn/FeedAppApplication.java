5abeb609d6e652c7388599a4757936a5d69b011d
DEDANDEUS <114673700+DEDANDEUS@users.noreply.github.com>
2022-11-11 14:07:40 -0800

    Created Classes

--- FeedApp/src/main/java/com/bptn/FeedAppApplication.java
+++ FeedApp/src/main/java/com/bptn/FeedAppApplication.java
@@ -1,8 +1,7 @@
 package com.bptn;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class FeedAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FeedAppApplication.class, args);
